f0192accc0325134de3af43c31cd977507164a2e
Daniel Pawsey <daniel.pawsey@live.co.uk>
2016-09-15 16:57:06 +0100

    Added BaseService.
    Stubbed BaseServiceImpl.
    
    Implemented BaseServiceImpl.java.
    
    Changed visibility of BaseServiceImpl private methods to protected.
    
    Annotated BaseServiceImpl as a service, to allow it to be registered as a bean by inheriting projects with controller impl classes which inherit from BaseServiceImpl.

--- src/main/java/com/pawsey/api/repository/BaseRepository.java
+++ src/main/java/com/pawsey/api/repository/BaseRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.repository.NoRepositoryBean;
 import java.io.Serializable;
 
 /**
- * Base repository class, intended to decouple from the database-accessing class.
+ * Base repository class, intended to decouple from the database-accessing class, and to follow the inheritence-pattern
+ * of this project set in {@link com.pawsey.api.service.com.pawsey.api.service.BaseService}.
  *
  * @param <T> The enntity-class to be accessed by this repository
  * @param <ID> The ID type of this object, e.g. {@link java.lang.Integer}.
---
+++ src/main/java/com/pawsey/api/service/impl/BaseServiceImpl.java
@@ -0,0 +1,109 @@
+package com.pawsey.api.service.impl;
+
+import com.pawsey.api.repository.BaseRepository;
+import com.pawsey.api.service.BaseService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.PersistenceException;
+import javax.security.auth.login.CredentialException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementation of {@link com.pawsey.api.service.BaseService}
+ */
+@Service
+public abstract class BaseServiceImpl<T, TRepository extends BaseRepository> implements BaseService<T> {
+    protected static final Log LOGGER = LogFactory.getLog(BaseServiceImpl.class);
+
+    @Autowired
+    protected
+    TRepository repository;
+
+    @Override
+    @Transactional
+    public T create(T entity) {
+        try {
+            T response = (T) repository.save(entity);
+            LOGGER.info(entity.getClass().getSimpleName()
+                    + " with hash of " + entity.toString() + " sucessfully had it's collections persisted.");
+            return response;
+        } catch(Exception e) {
+            String stateMessage = entity.getClass().getSimpleName()
+                    + " with hash of " + entity.toString() + " was not persisted successfully.";
+            PersistenceException persistenceException = new PersistenceException(stateMessage);
+            LOGGER.error(e);
+            throw persistenceException;
+        }
+    }
+
+    @Override
+    @Transactional
+    public T findById(Integer id) {
+        if (id != null) {
+            T response = (T) repository.findOne(id);
+
+            if (response != null) {
+                LOGGER.info(this.getClass().getSimpleName()
+                        + " retrieved an instance of it's principle entity with ID " + id);
+                return response;
+            } else {
+                String stateMessage = this.getClass().getSimpleName() + " could not retrieve an entity from "
+                        + repository.getClass().getSimpleName() + " with ID " + id;
+                DataRetrievalFailureException e = new DataRetrievalFailureException(stateMessage);
+                LOGGER.error(e);
+                throw e;
+            }
+        } else {
+            String stateMessage = "entity ID cannot be null.";
+            IllegalArgumentException e = new IllegalArgumentException(stateMessage);
+            LOGGER.error(e);
+            throw e;
+        }
+    }
+
+    @Override
+    @Transactional
+    public List<T> findAll() {
+        List<T> response = new ArrayList<>();
+
+        for (Object entity : repository.findAll()) {
+            response.add((T) entity);
+        }
+
+        return response;
+    }
+
+    @Override
+    @Transactional
+    public T update(T entity) throws CredentialException {
+        if (entity != null) {
+            T persistedEntity = (T) repository.save(entity);
+            return persistedEntity;
+        } else {
+            String stateMessage = entity.getClass().getSimpleName() + " update operation failed.";
+            IllegalArgumentException e = new IllegalArgumentException(stateMessage);
+            LOGGER.error(e);
+            throw e;
+        }
+    }
+
+    @Override
+    @Transactional
+    public void delete(T entity) {
+        try {
+            repository.delete(entity);
+
+            LOGGER.info(entity.getClass().getSimpleName()
+                    + " with hash of " + entity.getClass().toString() + " was deleted successfully.");
+        } catch (Exception e) {
+            LOGGER.error(e);
+            throw e;
+        }
+    }
+
+}
